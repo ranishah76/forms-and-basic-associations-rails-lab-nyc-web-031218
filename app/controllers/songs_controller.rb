0bb20e6e8bf23262a59c93f57257ce2b2eeff3da
Rani Shah <rani.shah@flatironschool.com>
2018-04-12 09:55:15 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,6 +9,7 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+    @genres = Genre.all
   end
 
   def create
@@ -23,6 +24,7 @@ class SongsController < ApplicationController
 
   def edit
     @song = Song.find(params[:id])
+    @genres = Genre.all
   end
 
   def update
@@ -47,7 +49,6 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :artist_name, :genre_id, note_contents: [])
   end
 end
-
